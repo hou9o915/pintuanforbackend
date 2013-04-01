87849a0f16547cab9d7b1a07e6a7b27f1527369c
侯群磊 <hou9o915@gmail.com>
2013-04-01 12:56:45 +0800

    added some models

--- controller.py
+++ controller.py
@@ -5,9 +5,10 @@ from models import *
 
 app = Bottle()
 
-@app.route('/')
+@app.get('/')
 def index():
-	return 'hello'
+	return '<a href="vida://slideshow?id=12346">Vida</a>'
+
 
 @app.post('/register')
 def register():
@@ -18,7 +19,6 @@ def register():
 		return tpl('template/failed.tpl',result=0,msg='username has already existed')
 	except User.DoesNotExist:
 		user = User.create(username=username,password=password)
-		user.save()
 		return tpl('template/user.tpl',result=1,user=user)
 
 
@@ -28,6 +28,12 @@ def login():
 	password = request.forms.get('password')
 	try:
 		user = User.get(username=username,password=password)
+		user.update_last_date()
 		return tpl('template/user.tpl',result=1,user=user)
 	except User.DoesNotExist:
 		return tpl('template/failed.tpl',result=0,msg='username or password error')
+
+
+#@app.get('/activity/recommend')
+#def get_ractivity_ecommend():
+	
--- models.py
+++ models.py
@@ -1,41 +1,91 @@
 #!/usr/bin/env python
 #-*-coding:UTF-8-*-
 
-import peewee
+from peewee import *
 from settings import *
 import datetime
 
-db = peewee.MySQLDatabase(MYSQL_DB, user=MYSQL_USER,passwd=MYSQL_PASSWORD)
+db = MySQLDatabase(MYSQL_DB, user=MYSQL_USER,passwd=MYSQL_PASSWORD)
 
-class BaseModel(peewee.Model):
+class BaseModel(Model):
+    created_date = DateTimeField(default=datetime.datetime.now)
+    del_flag = BooleanField(default=False)
     class Meta:
         database = db
 
 class User(BaseModel):
-    username = peewee.CharField(unique = True)
-    password = peewee.CharField()
-    gender = peewee.BooleanField()
-    token = peewee.CharField(unique = True)
-    last_date = peewee.DateTimeField(default=datetime.datetime.now)
-    created_date = peewee.DateTimeField(default=datetime.datetime.now)
+    username = CharField(unique = True)
+    password = CharField()
+    gender = BooleanField()
+    token = CharField(unique = True)
+    last_date = DateTimeField(default=datetime.datetime.now)
+    
 
     def __unicode__(self):
-        return self.username
+        return 'id:%-4dusername:%s'%(self.id,self.username)
 
-    @staticmethod
-    def create(**argv):
-    	argv['token'] = User.create_token(argv['username'])
-    	return User(**argv)
+    @classmethod
+    def create(self,**argv):
+        import hashlib
+    	argv['token'] = hashlib.sha224(argv['username']).hexdigest()
+    	return super(User,self).create(**argv)
 
     @staticmethod
     def create_token(str):
 		import hashlib
 		return hashlib.sha224(str).hexdigest()
 
+    def update_last_date(self):
+        now = datetime.datetime.now()
+        self.last_date = now
+        self.save()
+
+
+
+class Activity(BaseModel):
+    user = ForeignKeyField(User,related_name='activities_host')
+    pic = CharField()
+    title = CharField()
+    content = CharField()
+    activity_type = IntegerField()
+    duaration = IntegerField()
+    close_flag = BooleanField(default = False)
+
+
+    def __unicode__(self):
+        return 'user:%s   title:%s'%(self.user,self.title)
+
+class Picture(BaseModel):
+    activity = ForeignKeyField(Activity,related_name='pics')
+    pic_url = CharField()
+
+    def __unicode__(self):
+        return self.pic_url
+
+class Activity_Participant(BaseModel):
+    user = ForeignKeyField(User,related_name='activities_participated')
+    activity = ForeignKeyField(Activity,related_name='participants')
+
+class Message(BaseModel):
+    user_from = ForeignKeyField(User,related_name='messages_from')
+    user_to = ForeignKeyField(User,related_name='messages_to')
+    content = CharField()
+
+
 
 if __name__ == '__main__':
-	db.connect()
-	User.drop_table()
-	User.create_table()
-	#User.create(username="hou1",password="aa").save()
-	#User.create_table()
+    db.connect()
+    #Activity.drop_table()
+    #User.create_table()
+    #Activity.create_table()
+    #Picture.create_table()
+    #Message.create_table()
+    #user = User.get()
+    #Activity.create(user=user,title='cc').save()
+    #User.select().where(User.id == 1)
+    #print Activity.select().where(Activity.user==1).get()
+    #for activitiy in user.activities.offset(0).limit(1).order_by(Activity.id.desc()):
+    #    print activitiy
+    for i in range(300,1000000):
+        aa = str(i)
+        user = User.create(username=aa,password='222',gender=True)
